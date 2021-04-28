e981bca1ba2ce79f0ca036e5828d6fb30c86cf33
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-28 16:08:24 -0300

    alteração nos arquivos de autenticação do usuário para integrar o laravel passport.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use SoftDeletes, Notifiable;
+    use SoftDeletes, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
